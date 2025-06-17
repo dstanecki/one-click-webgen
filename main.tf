69e21bae3e9df8053d0c0c81de5cbf1b92391b39
Daniel Stanecki <staneckidaniel@gmail.com>
2025-06-17 13:24:45 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "tfstate836301532" # State file in BLOB
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "state-demo-secure" {
+  name     = "state-demo"
+  location = "eastus"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "dstaneckistaticwebsite"
+  resource_group_name      = azurerm_resource_group.state-demo-secure.name
+  location                 = azurerm_resource_group.state-demo-secure.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  static_website {
+    index_document = "index.html"
+    error_404_document = "index.html"
+  }
+}
+
+resource "azurerm_storage_blob" "html" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.storage.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  source                 = "index.html"
+  content_type           = "text/html"
+}
